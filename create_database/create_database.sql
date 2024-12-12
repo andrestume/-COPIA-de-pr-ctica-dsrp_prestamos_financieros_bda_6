223de00cd664b04a772253e6bf44a54c903210d3
andrestume <andres.tume@gmail.com>
2024-12-11 19:24:03 -0500

    11-12-2024

--- create_database/create_database.sql
+++ create_database/create_database.sql
@@ -1,2 +1,33 @@
 CREATE DATABASE dsrp_prestamos_financieros_6;
 GO
+
+USE dsrp_prestamos_financieros_6;
+GO
+
+--Creación de tabla personas_naturales
+CREATE TABLE personas_naturales(
+id INT PRIMARY KEY IDENTITY(1,1),
+numero_documento VARCHAR(20) UNIQUE NOT NULL,
+nombres VARCHAR(255) NOT NULL,
+apellido_paterno VARCHAR(255) NOT NULL,
+apellido_materno VARCHAR(255) NOT NULL,
+email VARCHAR(100) NOT NULL,
+celular VARCHAR(20) NOT NULL,
+direccion VARCHAR(255) NOT NULL
+);
+GO
+
+SELECT * FROM personas_naturales;
+
+EXEC sp_help personas_naturales;
+
+--Creación de tabla personas_juridicas
+CREATE TABLE personas_juridicas(
+id INT PRIMARY KEY IDENTITY(1,1),
+numero_documento VARCHAR(255) UNIQUE NOT NULL,
+razon_social VARCHAR(255) NOT NULL,
+email VARCHAR(100) NOT NULL,
+telefono VARCHAR(20) NOT NULL,
+direccion VARCHAR(255) NOT NULL
+);
+GO
