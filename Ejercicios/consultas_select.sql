8c7e913105aabea2845a6a5756f0cc72aec4847d
andrestume <andres.tume@gmail.com>
2024-12-18 15:28:59 -0500

    Upgrade consulta where

--- Ejercicios/consultas_select.sql
+++ Ejercicios/consultas_select.sql
@@ -1,6 +1,6 @@
 USE dsrp_prestamos_financieros_6
 
--- Igualdad simple: seleccionar DNI, nombre completo y email de todos los DAVID
+-- IGUALDAD SIMPLE: seleccionar DNI, nombre completo y email de todos los DAVID
 
 
 SELECT
@@ -17,3 +17,38 @@ SELECT
 	email
 FROM personas_naturales
 WHERE nombres='Juan'
+
+--Encontrar filas que contienen un valor como parte de una cadena
+--Seleccionar todas las personas naturales cuyo apellido paterno contengan la cadena 'ez'
+
+SELECT
+	numero_documento AS 'DNI',
+	CONCAT(nombres,' ',apellido_paterno, ' ', apellido_materno) AS 'Nombre_completo', ---si voy a reutilizar la consulta como un proceso, no dejar espacios
+	email
+FROM personas_naturales
+WHERE apellido_paterno LIKE('%ez%')
+
+--Ahora, los apellidos paternos que empiezan con 'R'
+SELECT
+	numero_documento AS 'DNI',
+	CONCAT(nombres,' ',apellido_paterno,' ',apellido_materno) AS 'Nombre completo',
+	email
+FROM personas_naturales
+WHERE apellido_paterno LIKE ('R%')
+
+--Ahora, los apellidos que terminan con 'S'
+SELECT
+	numero_documento AS 'DNI',
+	CONCAT(nombres,' ',apellido_paterno,' ',apellido_materno) AS 'Nombre_completo',
+	email
+FROM personas_naturales
+WHERE apellido_paterno LIKE ('%s')
+
+--Apellidos paternos que terminen con 'S' y cuyo apellido materno termine en 'A'
+SELECT
+	numero_documento AS 'DNI',
+	CONCAT(nombres,' ',apellido_paterno,' ',apellido_materno) AS 'Nombre completo',
+	email
+FROM personas_naturales
+WHERE apellido_paterno LIKE ('%s') AND apellido_materno LIKE ('%a')
+
